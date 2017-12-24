4f71387920cadd57cc6142c88063241bb93e6388
saikham <saikham.email@gmail.com>
2017-12-24 14:31:26 +0630

    edit mongodb url for mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
  // || 'mongodb://localhost:27017/TodoApp'
-// mongodb://saikham:saikham.mlab.123@ds163806.mlab.com:63806/saikham
+// mongodb://saikham:123mlab@ds163806.mlab.com:63806/saikham
 
 module.exports = {
 	mongoose
