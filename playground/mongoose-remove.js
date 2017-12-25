c6b49befbd8463407416e8d985d3c0d10e3d6506
saikham <saikham.email@gmail.com>
2017-12-25 12:24:10 +0630

    Add DELETE route /todos/:id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,15 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}) -> remove all
+
+// Todo.findOneAndRemove() -> doc back
+// Todo.findByIdAndRemove() -> doc back
+
+
+
+
+
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
  // || 'mongodb://localhost:27017/TodoApp'
-// mongodb://saikham:123mlab@ds163806.mlab.com:63806/saikham
+// mongodb://saikham:saikham@ds163806.mlab.com:63806/saikham
 
 module.exports = {
 	mongoose
